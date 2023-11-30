ea98df3da4234bb790ad33d447552755bc82e11c
balita <balitatsiory@gmail.com>
2023-11-30 22:00:25 +0100

    create project

---
+++ demo/src/test/java/railway/nba/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package railway.nba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
